4035a78684b2ec81a5d15e0846f3c3e4922f4908
soldevacs@gmail.com <soldev00>
2025-05-16 14:34:30 +0100

    added something

--- java.js
+++ java.js
@@ -60,17 +60,13 @@ function calculateBMI(weight, height) {
     return weight / (height * height);
 }
 
-let markWeight = 78;
-let markHeight = 1.68;
 let johnWeight = 92;
-let johnHeight = 1.95;
+let johnHeight = 1.69;
+let markWeight = 78;
+let markHeight = 1.69;
 
-let markBMI = calculateBMI(markWeight, markHeight);
-let johnBMI = calculateBMI(johnWeight, johnHeight);
 
-console.log(`Mark's BMI is: ${markBMI.toFixed(2)}`);
-console.log(`John's BMI is: ${johnBMI.toFixed(2)}`);
+ let johnBMI = calculateBMI(johnWeight, johnHeight);
+ let markBMI = calculateBMI(markWeight, markHeight);
 
-let markHigherBMI = markBMI > johnBMI;
-console.log(`Is Mark's BMI higher than John's? ${markHigherBMI}`);
-
+ console.log ('John BMI is : ' + johnBMI.toFixed(2));
